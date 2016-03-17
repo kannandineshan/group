01a5a1cdc644bfa79640a89795eed6fe2509274d
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-17 04:39:34 +0000

    aaa

--- new/assets/php/navigation.php
+++ new/assets/php/navigation.php
@@ -3,6 +3,39 @@
 function show_admin_home(){
 
 $htmlpage = <<< HTMLPAGE
+<!DOCTYPE html>
+
+<html lang="en">
+
+<!-- [START OF HEAD] --------------------------------------------------------------------------------------------->
+<head>
+    <meta charset="UTF-8">
+
+    <!-- WINDOW TAB TITLE -->
+    <title>Admin Home</title>
+
+    <!-- WINDOW TAB ICON -->
+    <link rel="shortcut icon" href="assets/images/tabicon.png" type="image/x-icon" />
+
+    <!-- CSS Stylesheet-->
+    <link rel="stylesheet" href="assets/css/adminhome.css" type="text/css">
+</head>
+<!-- [END OF HEAD] ----------------------------------------------------------------------------------------------->
+
+<!-- [START OF BODY] --------------------------------------------------------------------------------------------->
+<body>
+
+
+
+
+<!-- [START OF HEADER] ------------------------------------------------------------------------------------------->
+<header>
+
+    <!-- TOP PAGE BANNER -->
+    <section class="container" id="bannercontainer"></section>
+
+    <!-- NAVIGATION -->
+    <nav>
         <ul>
             <li><a class="active" href="#">Home</a></li>
             <li class="dropdown"><a href="#" class="dropbtn">User Login Setup</a>
@@ -20,6 +53,16 @@ $htmlpage = <<< HTMLPAGE
             <li style="float:right"><a href="#">Logout</a></li>
             <li style="float:right" ><a href="#">My Account</a></li>
         </ul>
+    </nav>
+</header>
+<!-- [END OF HEADER] ---------------------------------------------------------------------------------------------->
+
+
+
+
+
+</body>
+</html>
 
 HTMLPAGE;
 print $htmlpage;
