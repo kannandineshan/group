3713688cca6c9259049c574f2eb388353264246d
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-17 04:32:55 +0000

    aaa

--- new/adminhome.php
+++ new/adminhome.php
@@ -31,7 +31,23 @@
 
     <!-- NAVIGATION -->
     <nav>
-        <?php include 'navigation.php';?>
+        <ul>
+            <li><a class="active" href="#">Home</a></li>
+            <li class="dropdown"><a href="#" class="dropbtn">User Login Setup</a>
+                <div class="dropdown-content">
+                    <a href="#">Create User Login</a>
+                    <a href="#">Delete User Login</a>
+                </div>
+            </li>
+            <li class="dropdown"><a href="#" class="dropbtn">Report</a>
+                <div class="dropdown-content">
+                    <a href="#">Generate Report</a>
+                    <a href="#">Delete Report</a>
+                </div>
+            </li>
+            <li style="float:right"><a href="#">Logout</a></li>
+            <li style="float:right" ><a href="#">My Account</a></li>
+        </ul>
     </nav>
 </header>
 <!-- [END OF HEADER] ---------------------------------------------------------------------------------------------->
